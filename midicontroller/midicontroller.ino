b48ffed32b2da98696ed7462d8c99f226178cf20
sowis <jh040597@naver.com>
2023-02-06 16:26:24 +0900

    서스테인 페달 동작 구현

--- midicontroller/midicontroller.ino
+++ midicontroller/midicontroller.ino
@@ -10,18 +10,16 @@
 
 #define CHANAL 0
 
+#define SUSTAIN_PEDAL_PIN 0
 #define SUSTAIN_PEDAL 64
 
-void midi_send(const byte type, const byte chanal, const byte note, const byte velocity) {
-  midiEventPacket_t packet = {type, (type << 4) | chanal, note, velocity};
-  MidiUSB.sendMIDI(packet);
-  MidiUSB.flush();
-  MidiUSB.flush();
-}
+int last_sustain_pedal_value;
+
+void midi_send(const byte type, const byte chanal, const byte note, const byte velocity);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(57600);
+  last_sustain_pedal_value = map(analogRead(SUSTAIN_PEDAL_PIN), 0, 1023, 0, 127);
 }
 
 void loop() {
@@ -37,8 +35,22 @@ void loop() {
   //midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, 0);
   //delay(3000);
 
-  int value = analogRead(0);
+  /* 서스테인 페달 컨트롤러 */
+  int sustain_pedal_value = map(analogRead(SUSTAIN_PEDAL_PIN), 0, 1023, 0, 127);
+  if (sustain_pedal_value <= 3) {
+    sustain_pedal_value = 0;
+  }
   
-  Serial.println(value);
-  midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, map(value, 0, 1023, 0, 127));
+  if (last_sustain_pedal_value != sustain_pedal_value) {
+    last_sustain_pedal_value = sustain_pedal_value;
+    midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, sustain_pedal_value);
+  }
+  /********************/
+}
+
+void midi_send(const byte type, const byte chanal, const byte note, const byte velocity) {
+  midiEventPacket_t packet = {type, (type << 4) | chanal, note, velocity};
+  MidiUSB.sendMIDI(packet);
+  MidiUSB.flush();
+  MidiUSB.flush();
 }
