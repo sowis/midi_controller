72297e430ee987461ef994a955f7ae272902f570
sowis <jh040597@naver.com>
2023-02-06 16:14:18 +0900

    배선 참고자료 추가

--- midicontroller/midicontroller.ino
+++ midicontroller/midicontroller.ino
@@ -21,18 +21,24 @@ void midi_send(const byte type, const byte chanal, const byte note, const byte v
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(57600);
 }
 
 void loop() {
-  midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, 127);
-  delay(1000);
+  //midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, 127);
+  //delay(1000);
   
-  midi_send(NOTE_ON, CHANAL, 50, 64);
-  delay(100);
+  //midi_send(NOTE_ON, CHANAL, 50, 64);
+  //delay(100);
   
-  midi_send(NOTE_OFF, CHANAL, 50, 64);
-  delay(2000);
+  //midi_send(NOTE_OFF, CHANAL, 50, 64);
+  //delay(2000);
 
-  midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, 0);
-  delay(3000);
+  //midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, 0);
+  //delay(3000);
+
+  int value = analogRead(0);
+  
+  Serial.println(value);
+  midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, map(value, 0, 1023, 0, 127));
 }
