018903c6d8f656014a2f63e5d84dbc010e87a411
sowis <jh040597@naver.com>
2024-05-23 08:22:38 +0900

    변수명 수정

--- midicontroller/midicontroller.ino
+++ midicontroller/midicontroller.ino
@@ -17,13 +17,13 @@
 
 #define CHANAL 0
 
-int last_sustain_pedal_value;
+int sustain_pedal_value;
 
 void midi_send(const byte type, const byte chanal, const byte note, const byte velocity);
 
 void setup() {
   // put your setup code here, to run once:
-  last_sustain_pedal_value = map(analogRead(PIN_SUSTAIN_PEDAL), 0, 1023, 0, 127);
+  sustain_pedal_value = map(analogRead(PIN_SUSTAIN_PEDAL), 0, 1023, 0, 127);
 }
 
 void loop() {
@@ -42,14 +42,14 @@ void loop() {
   /************/
 
   /* 서스테인 페달 컨트롤러 */
-  int sustain_pedal_value = map(analogRead(PIN_SUSTAIN_PEDAL), 0, 1023, 0, 127);
-  if (sustain_pedal_value <= 3) {
-    sustain_pedal_value = 0;
+  int tmp_sustain_pedal_value = map(analogRead(PIN_SUSTAIN_PEDAL), 0, 1023, 0, 127);
+  if (tmp_sustain_pedal_value <= 3) {
+    tmp_sustain_pedal_value = 0;
   }
   
-  if (abs(last_sustain_pedal_value - sustain_pedal_value) > 1) { // 값이 달라질때만 신호 보냄
-    last_sustain_pedal_value = sustain_pedal_value;
-    midi_send(CONTROL_CHANGE, CHANAL, CC_SUSTAIN_PEDAL, sustain_pedal_value);
+  if (abs(sustain_pedal_value - tmp_sustain_pedal_value) > 1) { // 값이 달라질때만 신호 보냄
+    sustain_pedal_value = tmp_sustain_pedal_value;
+    midi_send(CONTROL_CHANGE, CHANAL, CC_SUSTAIN_PEDAL, tmp_sustain_pedal_value);
   }
   /********************/
 }
