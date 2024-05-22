a4cb2e52dec4eb6ee88ef4a9cfcb535312d7eb12
sowis <jh040597@naver.com>
2024-05-23 08:06:49 +0900

    서스테인 페달 값 떨림 제거

--- midicontroller/midicontroller.ino
+++ midicontroller/midicontroller.ino
@@ -1,3 +1,4 @@
+#include <HX710B.h>
 #include <frequencyToNote.h>
 #include <MIDIUSB.h>
 #include <MIDIUSB_Defs.h>
@@ -8,10 +9,13 @@
 #define NOTE_OFF       0x08
 #define CONTROL_CHANGE 0x0b
 
-#define CHANAL 0
+#define CC_BREATH_CONTROLLER 2
+#define CC_SUSTAIN_PEDAL 64
 
-#define SUSTAIN_PEDAL_PIN 1
-#define SUSTAIN_PEDAL 64
+#define PIN_SUSTAIN_PEDAL 1
+#define PIN_BREATH_CONTROLLER 2
+
+#define CHANAL 0
 
 int last_sustain_pedal_value;
 
@@ -19,11 +23,12 @@ void midi_send(const byte type, const byte chanal, const byte note, const byte v
 
 void setup() {
   // put your setup code here, to run once:
-  last_sustain_pedal_value = map(analogRead(SUSTAIN_PEDAL_PIN), 0, 1023, 0, 127);
+  last_sustain_pedal_value = map(analogRead(PIN_SUSTAIN_PEDAL), 0, 1023, 0, 127);
 }
 
 void loop() {
-  //midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, 127);
+  /* 사용 예제 */
+  //midi_send(CONTROL_CHANGE, CHANAL, CC_SUSTAIN_PEDAL, 127);
   //delay(1000);
   
   //midi_send(NOTE_ON, CHANAL, 50, 64);
@@ -32,18 +37,19 @@ void loop() {
   //midi_send(NOTE_OFF, CHANAL, 50, 64);
   //delay(2000);
 
-  //midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, 0);
+  //midi_send(CONTROL_CHANGE, CHANAL, CC_SUSTAIN_PEDAL, 0);
   //delay(3000);
+  /************/
 
   /* 서스테인 페달 컨트롤러 */
-  int sustain_pedal_value = map(analogRead(SUSTAIN_PEDAL_PIN), 0, 1023, 0, 127);
+  int sustain_pedal_value = map(analogRead(PIN_SUSTAIN_PEDAL), 0, 1023, 0, 127);
   if (sustain_pedal_value <= 3) {
     sustain_pedal_value = 0;
   }
   
-  if (last_sustain_pedal_value != sustain_pedal_value) {
+  if (abs(last_sustain_pedal_value - sustain_pedal_value) > 1) { // 값이 달라질때만 신호 보냄
     last_sustain_pedal_value = sustain_pedal_value;
-    midi_send(CONTROL_CHANGE, CHANAL, SUSTAIN_PEDAL, sustain_pedal_value);
+    midi_send(CONTROL_CHANGE, CHANAL, CC_SUSTAIN_PEDAL, sustain_pedal_value);
   }
   /********************/
 }
@@ -51,6 +57,5 @@ void loop() {
 void midi_send(const byte type, const byte chanal, const byte note, const byte velocity) {
   midiEventPacket_t packet = {type, (type << 4) | chanal, note, velocity};
   MidiUSB.sendMIDI(packet);
-  MidiUSB.flush();
   MidiUSB.flush();
 }
